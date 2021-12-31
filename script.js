5a5435c6f06a3f354474ce79c577dc6bfdca31f3
Ademir Candelario R <ademircandelario07@hotmail.com>
2021-12-30 23:30:52 -0600

    adding new Javascript functions

--- script.js
+++ script.js
@@ -10,13 +10,26 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed)
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "° C";
         document.querySelector(".humidity").innerText = "humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " Km/h";
-
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
-}
+};
+
+document.querySelector(".search button").addEventListener("click", function (){
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Desamparados");
